481169356205f7e6371350e12f1b9be9067a90b6
Admin <mynguyen8333@gmail.com>
2021-06-20 17:13:07 +0700

    Xong giao dien thu 2

--- app/src/main/java/com/example/myapplication/Screen02.java
+++ app/src/main/java/com/example/myapplication/Screen02.java
@@ -2,13 +2,30 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Screen02 extends AppCompatActivity {
-
+    ImageView imageView;
+    TextView textName,tvGia;
+    Coffe coffe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen02);
+        imageView = findViewById(R.id.img_screen2);
+        textName = findViewById(R.id.tvName2);
+        tvGia = findViewById(R.id.tvGia2);
+
+        Intent intent = getIntent();
+        if(intent!=null){
+            coffe = (Coffe) intent.getSerializableExtra("key1");
+            imageView.setImageResource(coffe.getImg());
+            textName.setText(coffe.getName());
+            tvGia.setText(coffe.getGia());
+
+        }
     }
 }
